d49ef324c8b4eb4e582cb48c8a746ea9c9a605dd
Sahib-Aujla <sahibpreet568@gmail.com>
2023-01-07 18:43:48 -0500

    Added Pagination functionality

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -2,15 +2,37 @@ import React,{useState} from 'react'
 import { AnimeList } from './constants';
 import { useGetAnimesQuery } from '../AnimeApi/AnimeApi'
 import { useSelector } from 'react-redux';
-
+import { Box, CircularProgress, Typography } from '@mui/material';
+import Pagination from './Pagination/Pagination';
 
 const Explore = () => {
-  const page=1;
+  const [page, setPage] = useState(1)
+  
   const { genreIdOrCategoryName, searchQuery }=useSelector((State)=>State.currentGenreOrCategory);
-  const {data}=useGetAnimesQuery({genreIdOrCategoryName,searchQuery,page});
+  const {data, error, isFetching}=useGetAnimesQuery({genreIdOrCategoryName,searchQuery,page});
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+  if (!data?.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">No Animes match that name.<br /> Search again for other title</Typography>
+      </Box>
+    );
+  }
+  if (error) {
+    return 'An error Occured....';
+  }
   console.log(data);
   return (
-    <div><AnimeList animes={data}/></div>
+    <div><AnimeList animes={data}/>
+      <Pagination currentPage={page} setPage={setPage} totalPages={100} />
+    </div>
+
   )
 }
 
